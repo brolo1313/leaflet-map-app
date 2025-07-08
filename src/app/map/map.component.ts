7a38f38f0b50259d82c1750d7cd5da8ab038fb1f
A.Yupin <brolo1341@gmail.com>
2025-07-08 09:21:17 +0300

    Implement map component with Leaflet integration; added initial map HTML structure and styles

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,6 +15,18 @@ interface RoutePoint {
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+  private map?: L.Map;
 
+  ngAfterViewInit(): void {
+    this.map = L.map('map', {
+      center: [48.3794, 31.1656],
+      zoom: 6,
+      zoomControl: true
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
 }
