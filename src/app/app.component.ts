e7135466b1d1134e8fce09299a5d22aafb6a81dd
A.Yupin <brolo1341@gmail.com>
2025-07-08 10:42:51 +0300

    hotFix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as L from 'leaflet';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leaflet-map-app';
+
+  constructor() {
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'assets/leaflet/images/marker-icon-2x.png',
+      iconUrl: 'assets/leaflet/images/marker-icon.png',
+      shadowUrl: 'assets/leaflet/images/marker-shadow.png'
+    });
+  }
 }
